b964ca7e8591538e171f2882c44509bd0a14c38b
Carlos Uziel Perez Malla <uziel@MAC.local>
2019-02-02 11:16:30 +0100

    Added warema api folder

--- warema_python_api/warema.py
+++ warema_python_api/warema.py
@@ -3,13 +3,20 @@ import time
 
 class WaremaBlind():
     """Class to control Warema window blinds.
-
+        (Local operation: time mode)
     """
     def up(self, seconds: float):
+        """Moves the window blind upwards.
+
+        Args:
+            seconds (float): Seconds to move.
+        """
         automationhat.relay.one.on()
         time.sleep(seconds)
         automationhat.relay.one.off()
         time.sleep(0.1)
+
+        # if blind has entered lock mode
         if seconds >= 2:
             automationhat.relay.two.on()
             time.sleep(0.1)
@@ -17,22 +24,19 @@ class WaremaBlind():
             time.sleep(0.1)
 
     def down(self, seconds: float):
+        """Moves the window blind downwards.
+
+        Args:
+            seconds (float): Seconds to move.
+        """
         automationhat.relay.two.on()
         time.sleep(seconds)
         automationhat.relay.two.off()
         time.sleep(0.1)
+
+        # if blind has entered lock mode
         if seconds >= 2:
             automationhat.relay.one.on()
             time.sleep(0.1)
             automationhat.relay.one.off()
             time.sleep(0.1)
-
-    def tilt_up(self, secs_up: float,
-                secs_down: float):
-        up(secs_up)
-        down(secs_down)
-
-    def tilt_down(self, secs_up: float,
-                  secs_down: float):
-        down(secs_down)
-        up(secs_up)
