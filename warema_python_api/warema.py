14f7e65d1ed128c70baa862bd0fe4e84d3379e0e
Carlos Uziel Perez Malla <uziel@MAC.local>
2019-02-02 15:30:43 +0100

    First try, removed comments

--- warema_python_api/warema.py
+++ warema_python_api/warema.py
@@ -1,4 +1,4 @@
-#import automationhat
+import automationhat
 import time
 
 class WaremaBlind():
@@ -26,17 +26,17 @@ class WaremaBlind():
         """
 
         self.position  = self.position - 10
-        #automationhat.relay.one.on()
+        automationhat.relay.one.on()
         time.sleep(seconds)
-        #automationhat.relay.one.off()
+        automationhat.relay.one.off()
         print("going up")
         time.sleep(0.1)
 
         # if blind has entered lock mode
         if seconds >= 2:
-            #automationhat.relay.two.on()
+            automationhat.relay.two.on()
             time.sleep(0.1)
-            #automationhat.relay.two.off()
+            automationhat.relay.two.off()
             time.sleep(0.1)
 
         print("finish up")
@@ -47,17 +47,17 @@ class WaremaBlind():
         Args:
             seconds (float): Seconds to move.
         """
-        #automationhat.relay.two.on()
+        automationhat.relay.two.on()
         print("going down")
         time.sleep(seconds)
 
         print("down !")
-        #automationhat.relay.two.off()
+        automationhat.relay.two.off()
         time.sleep(0.1)
 
         # if blind has entered lock mode
         if seconds >= 2:
-            #automationhat.relay.one.on()
+            automationhat.relay.one.on()
             time.sleep(0.1)
-            #automationhat.relay.one.off()
+            automationhat.relay.one.off()
             time.sleep(0.1)
