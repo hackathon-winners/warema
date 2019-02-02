f6eeb9ead3813cfa2dc069979ed6ad7383b067b5
Mira Keersmaekers <Mira@Miras-MacBook-Pro.local>
2019-02-02 14:57:05 +0100

    design draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,24 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import MotionDetector from "./components/MotionDetector/MotionDetector";
 import Mira from "./components/Mira/Mira";
 
+import styles from "./App.module.scss";
+
 const Index = () => <h2>Home</h2>;
 const Flo = () => <div><MotionDetector /></div>;
 
 export default function() {
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/mira">Mira</Link>
-            </li>
-            <li>
-              <Link to="/flo">Flo</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/" exact component={Index} />
-        <Route path="/mira" component={Mira} />
-        <Route path="/flo" component={Flo} />
+      <div className={styles.container}>
+        <div className={styles.motioncharts}>
+          <h2>Meeting productivity <span className={styles.focus}>• high</span></h2>
+        </div>
+        <div className={styles.videostream}></div>
+        <div className={styles.motionstream}></div>
+        <div className={styles.productivity}></div>
+        <div className={styles.participants}></div>
+        <div className={styles.timeschedule}></div>
+        <div className={styles.activitylog}></div>
       </div>
     </Router>
   );
