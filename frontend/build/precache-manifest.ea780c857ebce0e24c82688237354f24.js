91b8da30e60715b09d96a67d0507a8dea7e61aef
Florian Bauer <florian@moonshiner.at>
2019-02-03 01:57:18 +0100

    fix

---
+++ frontend/build/precache-manifest.ea780c857ebce0e24c82688237354f24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0af465dc7118f8d4508617427f6c8b77",
+    "url": "/static/media/icon-meeting-inactive.0af465dc.svg"
+  },
+  {
+    "revision": "f2b175bc7cc4f684a09c66ad08ff7d5d",
+    "url": "/static/media/icon-meeting-formal.f2b175bc.svg"
+  },
+  {
+    "revision": "abc2e609fc4817ab1c70a8196ab03cd9",
+    "url": "/static/media/icon-meeting-engaged.abc2e609.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7be1fc3fb9274310ab4b",
+    "url": "/static/js/main.7be1fc3f.chunk.js"
+  },
+  {
+    "revision": "023c75449536bb7cc20f",
+    "url": "/static/js/1.023c7544.chunk.js"
+  },
+  {
+    "revision": "7be1fc3fb9274310ab4b",
+    "url": "/static/css/main.cc69f5b9.chunk.css"
+  },
+  {
+    "revision": "d3f50dd744b3f2ba1926e16d90e87067",
+    "url": "/index.html"
+  }
+];
